58fe637440e7b304339e4566846e3f6e4bcfefcd
Garrett Schultz <garrett.schultz@datarobot.com>
2026-06-03 17:04:57 -0700

    update example outputs and remove use-proxy-protocol from ingress-nginx values

--- examples/public/outputs.tf
+++ examples/public/outputs.tf
@@ -17,3 +17,14 @@ output "dns_zone_name_servers" {
   description = "Name servers of the DNS zone. Create NS records for these with your domain registrar to delegate the zone."
   value       = module.datarobot_infra.dns_zone_name_servers
 }
+
+output "app_service_account_email" {
+  description = "Email of the GCP service account to use for workloads running in the cluster. This service account will have permissions to access the storage bucket and artifact registry."
+  value       = module.datarobot_infra.datarobot_service_account_email
+}
+
+output "app_service_account_key_b64" {
+  description = "Base64-encoded JSON key for the application service account. This can be used to authenticate to GCP from workloads running in the cluster, or for local development. Handle with care!"
+  value       = base64encode(module.datarobot_infra.datarobot_service_account_key)
+  sensitive   = true
+}
